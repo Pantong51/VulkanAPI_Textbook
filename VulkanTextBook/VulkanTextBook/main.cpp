4ceefff0c9a57bc9127c797d503bcecb113e2c9a
Pantong51 <Danpan.nightshift@gmail.com>
2018-09-07 18:52:54 -0500

    CH8

--- VulkanTextBook/VulkanTextBook/main.cpp
+++ VulkanTextBook/VulkanTextBook/main.cpp
@@ -9,6 +9,7 @@
 #include <cstring>
 #include "vulkan/vulkan_core.h"
 #include "vulkan/vk_platform.h"
+#include <map>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
@@ -56,6 +57,39 @@ private:
 	GLFWwindow * window;
 	VkInstance instance;
 	VkDebugUtilsMessengerEXT callback;
+	VkPhysicalDevice PhysicalDevice = VK_NULL_HANDLE;
+
+	struct QueueFamilyIndices
+	{
+		int graphicsFamily = -1;
+		bool isComplete()
+		{
+			return graphicsFamily >= 0;
+		}
+	};
+
+	QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device)
+	{
+		QueueFamilyIndices indices;
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+		int i = 0;
+		for (const auto& queueFamily : queueFamilies)
+		{
+			if (queueFamily.queueCount > 0 && queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+			{
+				indices.graphicsFamily = i;
+			}
+			if (indices.isComplete())
+			{
+				break;
+			}
+			i++;
+		}
+		return indices;
+	}
 
 	bool checkValidationLayerSupport()
 	{
@@ -169,6 +203,45 @@ private:
 		{
 			throw std::runtime_error("failed to create instance!");
 		}
+	}
+
+	void PickPhysicalDevice()
+	{
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+		if (deviceCount == 0)
+		{
+			throw std::runtime_error("Failed to find GPUs with Vulkan support!");
+		}
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+		for (const auto& device : devices)
+		{
+			if (isDeviceSuitable(device))
+			{
+				PhysicalDevice = device;
+				break;
+			}
+		}
+		if (PhysicalDevice == VK_NULL_HANDLE)
+		{
+			throw std::runtime_error("failed to find a suitable gpu!");
+		}
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device)
+	{
+
+		VkPhysicalDeviceProperties deviceProperties;
+		VkPhysicalDeviceFeatures deviceFeatures;
+		vkGetPhysicalDeviceProperties(device, &deviceProperties);
+		vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+		return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU && deviceFeatures.geometryShader;
+	}
+
+	int RateDeviceSuitability(VkPhysicalDevice device)
+	{
+		return 0;
 	}
 
 	std::vector<const char*> getRequiredExtensions()
